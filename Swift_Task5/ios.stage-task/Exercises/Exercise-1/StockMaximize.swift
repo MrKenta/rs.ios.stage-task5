96870ba6f114a230f4b1388111b086e1e0944ce7
Raman Krutsiou <>
2021-06-14 13:46:03 +0300

    Add solition for task1

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,36 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        if prices.isEmpty {
+            return 0
+        }
+        if prices.count == 1{
+            return 0
+        }
+        var shares = [Int]()
+        var balance = 0
+        for (index, price) in prices.enumerated() {
+            switch index {
+            case prices.count-1:
+                if price >= prices[index-1] && shares.count != 0{
+                    for item in shares {
+                        balance += price - item
+                    }
+                    shares.removeAll()
+                }
+            case 0..<prices.count-1:
+                if price > prices[index+1] && shares.count != 0{
+                    for item in shares {
+                        balance += price - item
+                    }
+                    shares.removeAll()
+                }
+                if price <= prices[index+1] {
+                    shares.append(price)
+                }
+            default:
+                break
+            }}
+        return balance
     }
 }
